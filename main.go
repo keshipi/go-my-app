4606df248aa206bc3b524b52d981a480e7d95e71
keshipi <keshipink@gmail.com>
2024-07-02 22:10:56 +0900

    map

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	basic_()
+	map_()
 }
 
 func basic_() {
@@ -15,4 +16,17 @@ func basic_() {
 	// varを省略した変数宣言
 	x := 1
 	fmt.Println(x)
+}
+
+func map_() {
+	// string型のキー、int型の値を持つmap定義
+	var m map[string]int
+	fmt.Println(m)
+
+	m1 := make(map[string]int)
+	m1["John"] = 21
+	m1["Bob"] = 18
+	m1["Mark"] = 33
+	fmt.Println(m1)
+
 }
