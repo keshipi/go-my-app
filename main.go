78bde9fb633aba793331cd0b39bcb22194d6acf6
keshipi <keshipink@gmail.com>
2024-07-03 22:34:12 +0900

    array

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ func main() {
 	// basic_()
 	// map_()
 	// const_()
-	iota_()
+	// iota_()
+	// condition_()
+	array_()
 }
 
 func basic_() {
@@ -77,4 +79,47 @@ func iota_() {
 	)
 	fmt.Println(Apple4, Orange4, Banana4) // 0 1 2
 	fmt.Println(Monkey, Elephant, Pig)    // 0 1 2
+}
+
+func condition_() {
+	// if x == 2 && y == 3 {
+	// 	fmt.Println("test")
+	// }
+
+	// if user, err := userName(); err == nil {
+	// 	fmt.Println(user.Name)
+	// } else {
+	// 	log.Println(err)
+	// }
+}
+
+func array_() {
+	// int型 長さ4の配列: 固定長
+	var a [4]int
+	a[0] = 1
+	fmt.Println(a)
+
+	// スライスは長さを指定しない: 可変長
+	var b []int
+	// b[0] = 1 -> そのままアクセスするとpanicが発生する
+	fmt.Println(b)
+
+	// 変数宣言も兼ねた初期化
+	c := make([]int, 3)
+	c[0] = 1
+	c[1] = 2
+	c[2] = 3
+	fmt.Println(c)
+	// 初期値代入の初期化
+	d := []int{1, 2, 3}
+	fmt.Println(d)
+	// スライスの伸長
+	d = append(d, 4, 5, 6)
+	fmt.Println(d, len(d), d[2:5])
+	// スライスから要素の削除
+	n := 1
+	// d = append(d[:n], d[n+1:]...)
+	// fmt.Println(d)
+	d = d[:n+copy(a[n:], a[n+1:])]
+	fmt.Println(d)
 }
